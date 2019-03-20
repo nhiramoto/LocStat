b3de0a9a2b2b1d0e01be910bb989d7d93430cf59
nhtoshiaki <nicolau.hiramoto@gmail.com>
2019-03-20 17:13:34 -0300

    Escrevendo mais testes e adicionando arquivo de configuração TravisCI.

---
+++ LocStat/tests/test_github.py
@@ -0,0 +1,16 @@
+"""
+Unit test cases for Github module (utils.github.py).
+"""
+
+import pytest
+from ..utils.github import Github
+
+
+class TestGithub:
+
+    def test_get_absolute_url(self):
+        assert type(Github.get_absolute_url('')) is str, \
+            'Returned value is not a string.'
+        assert Github.get_absolute_url('foo/bar') == 'https://github.com/foo/bar'
+        with pytest.raises(ValueError):
+            Github.get_absolute_url(10)
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # LocStat
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 # Visão Geral
 
 LocStat é um Web Scraper responsável por analisar repositórios públicos hospedados no Github e criar relatórios com a estatística de Linhas de Código (LOC - Lines Of Code) para cada tipo de arquivo.
