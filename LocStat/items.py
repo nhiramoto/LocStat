ef61f3efd88aefdfbe7da72b69dd4709c7998a39
nhtoshiaki <nicolau.hiramoto@gmail.com>
2019-03-23 19:16:37 -0300

    Implementando métodos para atualizar atributos dos itens.

--- LocStat/items.py
+++ LocStat/items.py
@@ -19,6 +19,49 @@ class DirectoryItem(FileItem):
     repository_name = scrapy.Field()
     children = scrapy.Field()
 
+    def update_lines_bytes(self):
+        """
+        Updates the number of lines and bytes in the subdirectory.
+        """
+        if 'children' in self.fields and type(self['children']) is list:
+            self['amount_lines'] = 0
+            self['amount_bytes'] = 0
+            for child in self['children']:
+                if isinstance(child, DirectoryItem):
+                    child.update_lines_bytes()
+                self['amount_lines'] += child['amount_lines']
+                self['amount_bytes'] += child['amount_bytes']
+
+    def index_extensions(self):
+        """
+        Creates an index with the total of lines and bytes for each file
+          type.
+        """
+        def index_extensions_recursively(dir_item, index):
+            if 'children' in dir_item.fields \
+                    and type(dir_item['children']) is list:
+                for child in dir_item['children']:
+                    if isinstance(child, TextFileItem) \
+                            and 'extension' in child.fields \
+                            and 'amount_lines' in child.fields \
+                            and 'amount_bytes' in child.fields:
+                        if child['extension'] in index:
+                            index[child['extension']]['amount_lines'] += \
+                                child['amount_lines']
+                            index[child['extension']]['amount_bytes'] += \
+                                child['amount_bytes']
+                        else:
+                            index[child['extension']] = {
+                                'amount_lines': child['amount_lines'],
+                                'amount_bytes': child['amount_bytes']
+                            }
+                    elif isinstance(child, DirectoryItem):
+                        index_extensions_recursively(child, index)
+
+        index = {}
+        index_extensions_recursively(self, index)
+        return index
+
 
 class TextFileItem(FileItem):
     extension = scrapy.Field()
